8952417598f45f35ebfb6d63ab60d513fc462d93
Anitayadav31 <Anitadec31@gmail.com>
2018-04-02 15:23:08 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-^from flask import Flask
+from flask import Flask
 import os
 
-app = Flask(__ame__)
+app = Flask(__name__)
 
 port = int(os.getenv("PORT"))
 
 @app.route('/')
 def hello_world():
-    return 'Welcome'
+    return 'Hello World'
 
-if __name__== '__main__':
-   #port = 1245
-   app.run(host='0.0.0.0'port=port)
+if __name__ == '__main__':
+    #port = 1245
+    app.run(host='0.0.0.0',port=port)
